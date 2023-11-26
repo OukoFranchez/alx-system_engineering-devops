c4a39b0cf861ae5918368af773f4e6bd09314268
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2023-11-27 02:43:42 +0300

    feat: add solution to task 3

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# client configuration file with puppet
+
+exec {'SSH configuration':
+    command => "echo 'Host 54.172.129.154\n\tPasswordAuthentication no\n\tIdentityFile ~/.ssh/school' >> /etc/ssh/ssh_config",
+    path -> '/usr/bin'
+}
