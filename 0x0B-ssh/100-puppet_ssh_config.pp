d8714e11bcd93810331fae7371114d6f3ea79409
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2023-11-27 02:50:30 +0300

    fix: correct syntax error

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,5 +2,5 @@
 
 exec {'SSH configuration':
     command => "echo 'Host 54.172.129.154\n\tPasswordAuthentication no\n\tIdentityFile ~/.ssh/school' >> /etc/ssh/ssh_config",
-    path -> '/usr/bin'
+    path => '/usr/bin'
 }
