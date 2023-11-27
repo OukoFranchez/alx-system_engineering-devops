3c6be04e3fb60da48b711852f8931d2d111195c9
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2023-11-27 03:00:55 +0300

    refactor task 1 with an alternative approach

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,14 @@
-# Install flask using pip3
+# script installing flask
 
-exec { 'install_flask':
-    command  => '/usr/bin/pip3 install flask==2.1.0'
+# Ensure 'pip3' is installed
+package { 'python3-pip':
+  ensure => 'installed',
 }
 
+# Install Flask using pip3 with the specified version
 package { 'flask':
-    ensure   => '2.1.0',  # Specify the desired version here
-    provider => 'pip3',  # Use the pip3 provider for managing Python packages
-    require  => Exec['install_flask']
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
 
