efde30fe08edb5beb8a8832f1f8ab70095d4a7be
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2024-01-02 22:05:30 +0300

    correct indentation errors

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,35 +1,21 @@
 #!/usr/bin/python3
 
 """
-Module for task 0
-It requests data from a RESTful API
-(https://jsonplaceholder.typicode.com)
-and returns information about an employees tasks
+    This module requests data from a RESTful API
+    (https://jsonplaceholder.typicode.com/)
+    and returns information about an employees tasks
 """
 
 from requests import get
 from sys import argv
 
+
 def todo_progress():
-    """
-    Fetches the progress of a to-do list for a specific employee from a RESTful API.
-
-    Args:
-        None
-
-    Returns:
-        None
-
-    Example Usage:
-        python script.py 1
-
-    This function can be used by running a Python
-    script with the employee ID as a command line argument.
-    It will fetch the to-do list progress for the employee with
-    the specified ID from the RESTful API and display the progress.
-    """
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
-    todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1])
+    """ fetches the todo list progress for an employee """
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'\
+               .format(argv[1])
+    todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'\
+               .format(argv[1])
 
     total_count = 0
     completed_count = 0
@@ -44,6 +30,10 @@ def todo_progress():
             completed_list += "\t {}\n".format(task.get('title'))
         total_count += 1
 
-    print("Employee {} is done with tasks({}/{}):".format(user_data.get('name'), completed_count, total_count))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user_data.get('name'), completed_count, total_count))
     print(completed_list, end="")
 
+
+if __name__ == "__main__":
+    todo_progress()
