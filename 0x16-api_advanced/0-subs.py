9268904576e4ab49af548d5fe348bffed3d83ef5
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2024-01-17 00:07:27 +0300

    refactor task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,9 +2,9 @@
 """
 Function that query subscribers on a given Reddit subreddit.
 """
-
-
 import requests
+
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
 
 def number_of_subscribers(subreddit):
@@ -16,11 +16,9 @@ def number_of_subscribers(subreddit):
     - int: The total number of subscribers on the subreddit.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
